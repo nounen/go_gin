9f61abd476afa0a907ac2270c90b7d3222954bdb
lazyou <1394139539@qq.com>
2018-05-24 22:26:08 +0800

    parameters in path

--- quick_start.go
+++ quick_start.go
@@ -8,10 +8,20 @@ import (
 func main()  {
 	router := gin.Default()
 
+	// quick start
 	router.GET("/ping", func (c *gin.Context)  {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
+	})
+
+
+	// parameters in path
+	// 路由正则更多案例: `"/user/:name/*action"`
+	router.GET("/user/:name", func (c *gin.Context)  {
+		name := c.Param("name")
+
+		c.String(http.StatusOK, "hello %s", name)
 	})
 
 	// listen and serve on 0.0.0.0:8080
