69d60ad6fa7604db4e9ef5424f69fd61e24b9a52
lazyou <1394139539@qq.com>
2018-06-05 22:33:26 +0800

    响应 -- JSON/XML/YAML响应

--- quick_start.go
+++ quick_start.go
@@ -97,6 +97,29 @@ func main() {
 		}
 	})
 
+	// 响应 -- JSON/XML/YAML响应
+	router.GET("/moreJSON", func(c *gin.Context) {
+		// You also can use a struct
+		var msg struct {
+			Name    string `json:"user" xml:"user"`
+			Message string
+			Number  int
+		}
+
+		msg.Name = "Lena"
+		msg.Message = "hey"
+		msg.Number = 123
+
+		// 注意 msg.Name 变成了 "user" 字段
+		// 以下方式都会输出 :   {"user": "Lena", "Message": "hey", "Number": 123}
+		// c.JSON(http.StatusOK, gin.H{"user": "Lena", "Message": "hey", "Number": 123})
+		// c.XML(http.StatusOK, gin.H{"user": "Lena", "Message": "hey", "Number": 123})
+		// c.YAML(http.StatusOK, gin.H{"user": "Lena", "Message": "hey", "Number": 123})
+		c.JSON(http.StatusOK, msg)
+		// c.XML(http.StatusOK, msg)
+		// c.YAML(http.StatusOK, msg)
+	})
+
 	// 1. isten and serve on 0.0.0.0:8080
 	router.Run()
 
