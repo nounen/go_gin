ca39fd73efc90f69be14f6b96a87088bcac3d3d2
lazyou <1394139539@qq.com>
2018-06-05 21:22:13 +0800

    format

--- quick_start.go
+++ quick_start.go
@@ -3,59 +3,57 @@ package main
 import (
 	"fmt"
 	"log"
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main()  {
+func main() {
 	router := gin.Default()
 
 	// quick start
-	router.GET("/ping", func (c *gin.Context)  {
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
-
 	// parameters in path
 	// 路由正则更多案例: `"/user/:name/*action"`
-	router.GET("/user/:name", func (c *gin.Context)  {
+	router.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 
 		c.String(http.StatusOK, "hello %s", name)
 	})
 
-
-	// querystring parameters 
+	// querystring parameters
 	// eg: http://localhost:8080/welcome?firstname=lin&lastname=moumou
-	router.GET("/welcome", func (c *gin.Context)  {
+	router.GET("/welcome", func(c *gin.Context) {
 		firstname := c.DefaultQuery("firstname", "Guest")
 		lastname := c.Query("lastname")
 
 		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
 	})
 
-
 	// Multipart/Urlencoded Form (建议使用 postman 把数据发过来)
-	router.POST("/form_post", func (c *gin.Context)  {
+	router.POST("/form_post", func(c *gin.Context) {
 		message := c.PostForm("message")
 		nick := c.DefaultPostForm("nick", "anonymous")
 
 		c.JSON(http.StatusOK, gin.H{
-			"status": "posted",
+			"status":  "posted",
 			"message": message,
-			"nick": nick,
+			"nick":    nick,
 		})
 	})
 
 	// Upload files
 	/*
-	curl -X POST http://localhost:8080/upload \
-		-F "file=@./README.md" \
-		-H "Content-Type: multipart/form-data"
+		curl -X POST http://localhost:8080/upload \
+			-F "file=@./README.md" \
+			-H "Content-Type: multipart/form-data"
 	*/
-	router.POST("/upload", func (c *gin.Context)  {
+	router.POST("/upload", func(c *gin.Context) {
 		file, _ := c.FormFile("file")
 
 		log.Println(file.Filename)
@@ -68,11 +66,10 @@ func main()  {
 	// Multiple files
 	// 多文件上传. 略
 
-
 	// Grouping routes
 	v1 := router.Group("/v1")
 
-	v1.GET("/user/:name", func (c *gin.Context)  {
+	v1.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 
 		c.String(http.StatusOK, "v1 route group: %s", name)
